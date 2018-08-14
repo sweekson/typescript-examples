d40029561d58eda45c3e3ea0de36c9294f5f9f33
Wilson <sweekson@gmail.com>
2018-08-14 14:05:51 +0800

    update error message

--- classes/13. Type parameters as constraints.ts
+++ classes/13. Type parameters as constraints.ts
@@ -10,4 +10,4 @@ function assign<T extends U, U>(target: T, source: U): T {
 
 let x = { a: 1, b: 2, c: 3, d: 4 };
 assign(x, { b: 10, d: 20 });
-assign(x, { e: 0 });  // Error
+assign(x, { e: 0 });  // Error: Not assignable
